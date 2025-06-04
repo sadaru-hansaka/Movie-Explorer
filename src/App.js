5bf8a08ed59b2558a6c137fa2ed8c893dd467685
Sadaru Hansaka <sandaruhansaka0828@gmail.com>
2025-06-04 23:03:26 +0530

    Switch to HashRouter for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React,{useState,useMemo,useEffect} from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import HomeScreen from './screens/HomeScreen';
 import MovieDetails from './screens/MovieDetails';
